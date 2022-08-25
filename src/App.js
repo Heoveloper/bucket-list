4068087abb974abfcabc57ba141f56735d8c0345
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-25 16:53:29 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme'
+import Input from './components/Input';
+import EventButton from './components/EventButton';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({theme}) => theme.background};
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+  /* width: ${({width}) => width - 40}px; */
+  width: 90%;
+  background-size: ${({width}) => width - 40}px;
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({theme}) => theme.text};
+  background-color: ${({theme}) => theme.itemBackground};
+  align-self: center;
+  text-align: center;
+  margin: 0px 20px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>HELLO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={theme.background}
+        />
+        <Title>버킷 리스트</Title>
+        <Input placeholder="+항목추가"/>
+        <EventButton/>
+      </Container>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/EventButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+const EventButton = () => {
+    const _onPress = () => console.log('이거 누르면 완료항목 전체삭제');
+
+    return (
+        <TouchableOpacity
+            style={{
+                backgroundColor: '#101010',
+            }}
+            onPress={_onPress}
+        >
+            <Text style={{color: '#cfcfcf', fontSize: 35,}}>완료항목 전체삭제</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default EventButton;
